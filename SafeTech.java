a9658c85b51f9749be2d7545c9d999a51e69f163
VilmaJimenez <vjimenezp@miumg.edu.gt>
2025-09-19 21:16:14 -0600

    Add files via upload
    Signed-off-by: VilmaJimenez <vjimenezp@miumg.edu.gt>

---
+++ SafeTech.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package miumg.edu.gt.safetech.main;
+
+/**
+ *
+ * @author Julissa
+ */
+public class SafeTech {
+    public static void main(String[] args) {
+         System.out.println(" vj");
+    }
+    
+}
