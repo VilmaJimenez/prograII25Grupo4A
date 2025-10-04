16b7b20702e3368954d7bf4d10401b8691ff5bba
Isaac Ventura <eventurag3@miumg.edu.gt>
2025-10-04 16:45:44 -0600

    Final

--- SafeTech2/target/generated-sources/annotations/gt/edu/miumg/bd/Factura_.java
+++ SafeTech2/target/generated-sources/annotations/gt/edu/miumg/bd/Factura_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-10-04T16:19:12", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-10-04T16:44:23", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Factura.class)
 public class Factura_ { 
 
